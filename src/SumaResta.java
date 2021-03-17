374af5cb03f299660a21ac4fef40189dc2a1a06f
angel <angel@DESKTOP-1J61345>
2021-03-17 12:04:30 +0100

    SUm

---
+++ src/SumaResta.java
@@ -0,0 +1,11 @@
+
+public class SumaResta {
+	public static void main(String args[]) {
+		int num1 = 15;
+		int num2 = 10;
+		//Completa los sysos
+		System.out.println("La suma de ambos números da: " );
+		System.out.println("La resta de ambos números da: ");
+	}
+
+}
